7a19cbc7d2fa2e4310d3d28f7bd35f9684717dd9
ranggaeka <rangga.parkur@gmail.com>
2016-02-23 16:50:05 +0700

    tambah feature user aktiv

--- system/login_service.php
+++ system/login_service.php
@@ -4,33 +4,36 @@ if (isset($_POST['btn_login'])) {
 	$userN = $_POST['username'];
 	$pass = $_POST['password'];
 
-	$sql = mysql_query("SELECT * FROM user where username = '$userN' and password = '$pass' ");
-	$arr = mysql_fetch_array($sql);
-
-	$id = $arr['id'];
-	$password = $arr['password'];
-	$username = $arr['username'];
-	$jabatan = $arr['jabatan'];
-	$nama = $arr['nama'];
-	// $role = $arr['has_role'];
-
-
-	//echo "atas".$userN."<br/>";
-	//echo "atas".$pass."<br/>";
-	//echo "bawah".$username."<br/>";
-	//echo "bawah".$password."<br/>";
-	if (($userN == $username && $pass == $password) && ($userN != "" && $pass != "")) {
-    
-		session_start();
-		$_SESSION['username'] = $username;
-		$_SESSION['password'] = $password;
-		$_SESSION['user_sid'] = $id;
-		$_SESSION['nama'] = $nama;
-		$_SESSION['jabatan'] = $jabatan;
-    
-		echo "<script> window.location.href='../form/halaman_utama.php?page=home';</script>";
+	$cekSign = mysql_num_rows(mysql_query("select * from user where is_active = 1 and username = '".$userN."' "));
+	if ($cekSign > 0) {
+		echo "<script>alert('User `".$userN."` sedang login , Silahkan gunakan user lain ! '); window.history.back();</script>";
 	} else {
-		echo "<script> alert('Email atau password anda belum terdaftar, silahkan ulangi kembali! '); window.history.back();</script>";
+		$sql = mysql_query("SELECT * FROM user where username = '$userN' and password = '$pass' ");
+		$arr = mysql_fetch_array($sql);
+
+		$id = $arr['id'];
+		$password = $arr['password'];
+		$username = $arr['username'];
+		$jabatan = $arr['jabatan'];
+		$nama = $arr['nama'];
+	
+		if (($userN == $username && $pass == $password) && ($userN != "" && $pass != "")) {
+			$signOn = mysql_query("update user set is_active = 1 where username = '".$username."' ");
+			if ($signOn) {
+				session_start();
+				$_SESSION['username'] = $username;
+				$_SESSION['password'] = $password;
+				$_SESSION['user_sid'] = $id;
+				$_SESSION['nama'] = $nama;
+				$_SESSION['jabatan'] = $jabatan;
+				echo "<script> window.location.href='../form/halaman_utama.php?page=home';</script>";
+			} else {
+				echo "<script> alert('MYSQL ERROR could not execute update table `user` '); window.history.back();</script>";
+			}
+			
+		} else {
+			echo "<script> alert('Email atau password anda belum terdaftar, silahkan ulangi kembali! '); window.history.back();</script>";
+		}
 	}
 }
 
--- system/logout_service.php
+++ system/logout_service.php
@@ -1,6 +1,11 @@
 <?php
+include 'config_service.php';
 session_start();
-session_destroy();
-
-echo "<script>window.location.href='../'</script>";
+$signOut = mysql_query("update user set is_active = 0 where username = '".$_SESSION['username']."' ");
+if ($signOut) {
+	session_destroy();
+	echo "<script>window.location.href='../'</script>";
+} else {
+	echo "<script> alert('MYSQL ERROR could not execute update table `user` ');</script>";
+}
 ?>
